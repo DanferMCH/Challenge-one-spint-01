d09abc9bfe37fe9df087d1a1015878acb8112666
Danfer Martinez <martinezchavezdanfer@gmail.com>
2023-07-28 03:07:59 -0500

    Revert "Revert "conversion de moneta terminado""
    This reverts commit 4cbac8dbd0c50f1595c52453887ec7dae64d04d1.

---
+++ src/pe/com/convertidormoneda/principal/principal.java
@@ -0,0 +1,18 @@
+package pe.com.convertidormoneda.principal;
+
+import pe.com.convertidormoneda.vista.Jframe_inicio;
+import pe.com.convertidormoneda.vista.Jframe_menu_conversion;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		Jframe_menu_conversion inicio = new Jframe_menu_conversion();
+		inicio.setVisible(true);
+		
+		
+		
+
+	}
+
+}
